ad79dbd3b2286cf74ad70c2ad59d81c8b2abd584
guanty <905733858@qq.com>
2024-05-27 21:17:10 +0800

    1

--- tuling-common-sms/src/main/java/com/tuling/common/sms/service/SmsService.java
+++ tuling-common-sms/src/main/java/com/tuling/common/sms/service/SmsService.java
@@ -16,25 +16,22 @@ public interface SmsService  {
 
      String PHONE_NUM_TIMES_REDIS_PREFIX="phoneNumTimes:%s";
 
-     String PHONE_NUM_CODE_REDIS_PREFIX="phoneNumCode:%s";
 
     String send(String[] phoneNumSet,String[] templateParamSet,String templateId );
 
 
-   default String sendPhoneNumCode(String phoneNum,String code,String templateId ){
+   default String sendPhoneNumCode(String codeKey,String phoneNum,String code,String templateId ){
        RedisTemplate<String, Object> redisTemplate = SpringUtils.getBean("jacksonRedisTemplate");
 
-       // Redis key for the phone number
-       String redisKey = String.format(PHONE_NUM_TIMES_REDIS_PREFIX, phoneNum);
-
+       String timesKeys=String.format(PHONE_NUM_TIMES_REDIS_PREFIX,phoneNum);
        // Check how many times the code has been sent today
-       Integer sendCount = (Integer) redisTemplate.opsForValue().get(redisKey);
+       Integer sendCount = (Integer) redisTemplate.opsForValue().get(timesKeys);
        if (sendCount == null) {
            sendCount = 0;
        }
 
        if (sendCount >= SEND_MAX_TIMES_ONE_DAY) {
-           throw new ServiceException("当天发送次数过多");
+           throw new ServiceException(String.format("手机号码：%s当天发送次数过多",phoneNum));
        }
 
        // Send the SMS code
@@ -45,20 +42,17 @@ public interface SmsService  {
        Date now = DateUtil.date();
        Date endOfDay = DateUtil.endOfDay(now);
        long secondsUntilEndOfDay = DateUtil.between(now, endOfDay, DateUnit.SECOND);
-       redisTemplate.opsForValue().set(redisKey, sendCount, secondsUntilEndOfDay, TimeUnit.SECONDS);
 
-       // Store the verification code with a 1-minute expiry
-       String codeKey = String.format(PHONE_NUM_CODE_REDIS_PREFIX,phoneNum);
+       redisTemplate.opsForValue().set(timesKeys, sendCount, secondsUntilEndOfDay, TimeUnit.SECONDS);
+
        redisTemplate.opsForValue().set(codeKey, code, 1, TimeUnit.MINUTES);
 
        return result;
    }
 
 
-   default boolean checkPhoneNumCode(String phoneNum,String code){
+   default boolean checkPhoneNumCode(String codeKey,String code){
        RedisTemplate<String, Object> redisTemplate = SpringUtils.getBean("jacksonRedisTemplate");
-       // Redis key for the verification code
-       String codeKey = String.format(PHONE_NUM_CODE_REDIS_PREFIX, phoneNum);
 
        // Get the stored code from Redis
        String storedCode = (String) redisTemplate.opsForValue().get(codeKey);
